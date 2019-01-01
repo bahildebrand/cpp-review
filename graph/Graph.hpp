72629ee2cb33582117d95fecd2a09475004bac53
Blake Hildebrand <baugusthildebrand@gmail.com>
2019-01-01 13:18:31 -0600

    Djikstra implementation

--- graph/Graph.hpp
+++ graph/Graph.hpp
@@ -1,9 +1,11 @@
 #ifndef GRAPH__
 #define GRAPH__
 
+#include <climits>
 #include <vector>
 #include <utility>
 #include <iostream>
+#include <queue>
 
 using namespace std;
 
@@ -14,7 +16,6 @@ template <class T> class Graph {
                 vector<pair<int,int>> edges;
                 T data;
         };
-
     private:
         vector<Node> *nodes;
 
@@ -62,11 +63,47 @@ template <class T> void Graph<T>::printGraph() {
     }
 }
 
+//Djikstra
 template <class T> vector<int> Graph<T>::shortestPath(int start, int end) { 
-    vector<int> result;
-    vector<bool> visited(this->nodes->size(), false);
+    vector<int> dist(this->nodes->size(), INT_MAX);
+    vector<int> parent(this->nodes->size());
+    priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
 
+    dist[start] = 0;
+    pq.push(make_pair(0, start)); 
     
+    while(!pq.empty()) {
+        int u = pq.top().second;
+        pq.pop();
+
+        vector<pair<int,int>>::iterator it;
+
+        vector<pair<int,int>> edgeList = this->nodes->at(u).edges; 
+        for(it = edgeList.begin(); it != edgeList.end(); ++it ) {
+            int vert = (*it).first;
+            int weight = (*it).second;
+
+            if(dist[u] + weight < dist[vert]){
+                dist[vert] = dist[u] + weight; 
+                pq.push(make_pair(dist[vert], vert));
+                parent[vert] = u; 
+            }
+        }        
+    }
+
+    cout << "Shortest Path:" << endl;
+    for(int i = 0; i < dist.size(); i++) {
+        cout << "E: " << i << " Dist: " << dist[i] << endl;
+    }
+
+    vector<int> result;
+    int p = parent[end];
+    while(p != start) {
+        result.push_back(p);
+        p = parent[p];
+    }
+
+    return result;
 }
 
 #endif // GRAPH__
--- graph/driver.cpp
+++ graph/driver.cpp
@@ -1,4 +1,6 @@
 #include "Graph.hpp"
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -9,7 +11,15 @@ int main(void) {
     g.addEdge(0,2,5);
     g.addEdge(1,2,1);
     g.addEdge(2,3,4);
-    g.addEdge(2,4,2);
-    g.addEdge(3,4,1);
+    g.addEdge(2,3,2);
+    g.addEdge(3,3,1);
+    g.addEdge(2,1,1);
+    g.addEdge(3,1,4);
+    g.addEdge(2,1,5);
+    
     g.printGraph();
+    vector<int> result = g.shortestPath(0, 3);
+    for(vector<int>::iterator it = result.begin(); it != result.end(); ++it){
+        cout << *it << endl;
+    }
 }
